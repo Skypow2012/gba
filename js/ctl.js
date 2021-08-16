1fbcc153fac595258d3c77c04be1790adc62e897
maxmon <541182180@qq.com>
2021-08-16 23:34:19 +0800

    feat: 支持全屏、屏幕自适应

--- js/ctl.js
+++ js/ctl.js
@@ -41,6 +41,17 @@ function startSetCtl(idx) {
 	else if (idx < 16) user = 'P2'
 }
 
+function addCtl(ctl, idx, box) {
+	const tr = document.createElement('tr');
+	let ctlTar = (ctlTars[idx] || '').split('|')[0];
+	ctlTar = keyMaps[ctlTar] || ctlTar;
+	tr.innerHTML = `<td>${ctl}</td><td class="ctlSet" onmouseover={startSetCtl(${idx})}>${ctlTar}</td>`
+	if (ctlTar === '双击') {
+		tr.ondblclick = changeScreen;
+	}
+	box.appendChild(tr)
+}
+
 function renderCtlList() {
 	const boxId = 'mapping'
 	const box = document.getElementById(boxId);
@@ -48,11 +59,7 @@ function renderCtlList() {
 	box.onmouseleave = () => {
 		delete window.ctlIdx;
 	}
-	ctls.map((ctl, idx) => {
-		const tr = document.createElement('tr');
-		let ctlTar = (ctlTars[idx] || '').split('|')[0];
-		ctlTar = keyMaps[ctlTar] || ctlTar;
-		tr.innerHTML = `<td>${ctl}</td><td class="ctlSet" onmouseover={startSetCtl(${idx})}>${ctlTar}</td>`
-		box.appendChild(tr)
-	})
+	ctls.map((ctl, idx) => addCtl(ctl, idx, box))
+	ctlTars[999] = '双击'
+	addCtl('全屏', 999, box);
 }
